1b44b777e743be593932b4384efd62b7130c13eb
Max <44373521+maxwelldemaio@users.noreply.github.com>
2020-01-24 14:58:07 -0500

    Update ccnValidation.c

--- ccnValidation.c
+++ ccnValidation.c
@@ -2,18 +2,25 @@
 #include <cs50.h>
 #include <math.h>
 
+// Check sum function for card validity
 void check(long long ccn)
 {
     long long ccn1;
     int nDigits;
     int i;
+    int j;
     int iter;
     long long n;
     int sum;
+    int sum2;
+    sum = 0;
+    sum2 = 0;
+    // Determine the number of digits of the credit card
     nDigits = log10(ccn) + 1;
     printf("Number of digits = %d\n", nDigits);
-    sum = 0;
     
+    // Multiply every other digit by 2, starting with the number’s second-to-last digit
+    // Then add those products’ digits together
     for(i = 1; i < nDigits; i++)
     {
         iter = powf(10, i);
@@ -26,11 +33,22 @@ void check(long long ccn)
             }
             else
             {
-                sum = sum + (2* n);
+                sum = sum + (2 * n);
             }
-             printf("sum = %d\n", sum);
-        }   
+            printf("sum = %d\n", sum);
+        }
     }
+    // Sum of digits that weren't multiplied by 2
+    for(j = 0; j < nDigits; j++)
+    {
+        if(j % 2 == 0)
+        {
+            iter = powf(10, j);
+            n = (ccn/iter) % 10;
+            sum2 = sum2 + n;
+        }
+    }
+    printf("sum2 = %d\n", sum2);
 }
 
 int main(void)
